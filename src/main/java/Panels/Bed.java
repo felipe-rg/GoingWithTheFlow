1d93b9a499725070c48c62a77f68009ea4eb149b
feliperodrigosmac <felipe.rodrigo.g@icloud.com>
2020-12-13 16:58:58 +0100

    Beds in topography

---
+++ src/main/java/Panels/Bed.java
@@ -0,0 +1,28 @@
+package Panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Bed extends JButton{
+    String BedId;
+    char status;    //Character saying if bed is free (f), if it is occupied (o) or closed (c)
+
+    public Bed(String BedId, int x, int y, char status){
+        this.status = status;
+        this.BedId = BedId;
+
+        //Editing button
+        this.setText(BedId);
+        this.setBounds(x, y, 75, 140);
+
+        //Changing Color
+        this.setOpaque(true);
+        this.setBackground(Color.RED);
+
+
+    }
+
+
+
+
+}
