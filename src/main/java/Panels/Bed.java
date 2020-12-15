d6e5ef783d7e29ab08ca5ad73857aa61013838a1
clararodrigo <cr418@ic.ac.uk>
2020-12-15 12:42:50 +0000

    added clicking function buttons, added age and gender to Bed category to try printing info

--- src/main/java/Panels/Bed.java
+++ src/main/java/Panels/Bed.java
@@ -2,26 +2,52 @@ package Panels;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Bed extends JButton{
     String BedId;
     char status;    //Character saying if bed is free (f), if it is occupied (o) or closed (c)
+    Integer age;
+    char gender;
 
-    public Bed(String BedId, int x, int y, char status){
-        this.status = status;
+    public Bed(String BedId, char status, Integer x, Integer y, Integer age, char gender) {
         this.BedId = BedId;
+        this.status = status; //f = full, e = empty, c = closed
+        this.gender = gender;
+        this.age = age;
 
-        //Editing button
         this.setText(BedId);
-        this.setBounds(x, y, 75, 140);
-
-        //Changing Color
+        this.setBounds(x, y, 70, 140);
         this.setOpaque(true);
-        this.setBackground(Color.RED);
-
-
+        if(status == 'f'){setBackground(Color.RED); }
+        if(status == 'e'){setBackground(Color.GREEN);}
+        if(status == 'c'){setBackground(Color.YELLOW);}
     }
 
+    public String getID(){
+        return this.BedId;
+    }
+
+    public Integer getAge(){
+        return this.age;
+    }
+
+    public char getGender(){
+        return this.gender;
+    }
+
+    public void makeFull(){
+        if(this.status == 'e'){
+            this.status = 'f';
+        }
+    }
+
+    public void makeEmpty(){
+        if(this.status == 'f' || this.status == 'c'){
+            this.status = 'e';
+        }
+    }
 
 
 
--- src/main/java/Panels/Topography.java
+++ src/main/java/Panels/Topography.java
@@ -2,10 +2,10 @@ package Panels;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Topography extends JPanel{
-    //private ArrayList<Bed> bedList;
     Bed bed1;
     Bed bed2;
     Bed bed3;
@@ -15,34 +15,42 @@ public class Topography extends JPanel{
     Bed bed7;
     Bed bed8;
 
-
-    public Topography(){
-        this.setPreferredSize(new Dimension(900,700));
-        this.setBackground(Color.white);
+    public Topography() {
+        this.setBackground(Color.WHITE);
+        this.setPreferredSize(new Dimension(100, 70));
         this.setLayout(null);
 
-        setupBeds();
-
-
-    }
-
-    public void setupBeds(){
-        this.bed1 = new Bed("1", 100, 100, 'f');
-        this.bed2 = new Bed("2", 300, 100, 'f');
-        this.bed3 = new Bed("3", 500, 100, 'f');
-        this.bed4 = new Bed("4", 700, 100, 'f');
-        this.bed5 = new Bed("5", 100, 400, 'f');
-        this.bed6 = new Bed("6", 300, 400, 'f');
-        this.bed7 = new Bed("7", 500, 400, 'f');
-        this.bed8 = new Bed("8", 700, 400, 'f');
-
+        bed1 = new Bed("1",'f',100, 200, 28, 'f');
         this.add(bed1);
+        bed2 = new Bed("2",'e',300, 200,0,'x');
         this.add(bed2);
+        bed3 = new Bed("3",'c',500, 200,0,'x');
         this.add(bed3);
+        bed4 = new Bed("4",'e',700, 200,0,'x');
         this.add(bed4);
+        bed5 = new Bed("5",'e',100, 400,0,'x');
         this.add(bed5);
+        bed6 = new Bed("6",'f',300, 400,65,'m');
         this.add(bed6);
+        bed7 = new Bed("7",'f',500, 400,19,'m');
         this.add(bed7);
+        bed8 = new Bed("8",'e',700, 400,0,'x');
         this.add(bed8);
+
+        bed1.addActionListener(evt -> JOptionPane.showMessageDialog(bed1, "Patient: "+bed1.getID()+ "\nAge: " + bed1.getAge() + "\nGender: "+bed1.getGender()));
+
+        bed6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                JOptionPane.showMessageDialog(bed6, "Patient: "+bed6.getID()+ "\nAge: " + bed6.getAge() + "\nGender: "+bed6.getGender());
+            }
+        });
+
     }
+
+
+
+
+
+
 }
