1229bde0902aa9d8ffb10f2da99edf028e10561b
nv2917 <nv2917@ic.ac.uk>
2020-12-21 16:48:37 +0000

    Client ver 1.0 including: doGet doPost doPut doDelete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main (String[] args) {
 
-        Client c = new Client();
-        /*Patient pi = new Patient("IB","F","leg",false);
-        c.makePostRequest(pi);*/
-
-        /*ArrayList<Patient> patients = c.makeGetRequest("*","patients","id%3E40");
-
-        for(Patient p:patients) {
-            System.out.println(p.getId()+' '+p.getNameInitials()+' '+p.getCurrentWardId()+' '+p.getCurrentBedId());
-        }*/
-
-        //c.makeDeleteRequest("patients","id%3E46");
-
-        //c.makePutRequest("patients","nameinitials='ael'","id%3E43");
 
     }
 
