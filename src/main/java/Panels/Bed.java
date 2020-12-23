71ae5e0fc20dc764a4290de23cec2fd7966c4cd2
clararodrigo <cr418@ic.ac.uk>
2020-12-23 17:19:56 +0100

    Made edit bed information functions

--- src/main/java/Panels/Bed.java
+++ src/main/java/Panels/Bed.java
@@ -4,13 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Bed extends JButton{
     String BedId;
     char status;    //Character saying if bed is free (f), if it is occupied (o) or closed (c)
     Integer age;
     char gender;
-    JFrame infoFrame = new JFrame();
+    private JFrame infoFrame = new JFrame();
 
 
     public Bed(String BedId, char status, Integer x, Integer y, Integer age, char gender) {
@@ -30,11 +31,9 @@ public class Bed extends JButton{
     public String getID(){
         return this.BedId;
     }
-
     public Integer getAge(){
         return this.age;
     }
-
     public char getGender(){
         return this.gender;
     }
@@ -44,17 +43,15 @@ public class Bed extends JButton{
             this.status = 'f';
         }
     }
-
     public void makeEmpty(){
         if(this.status == 'f' || this.status == 'c'){
             this.status = 'e';
         }
     }
-
     public void setAge(Integer age){
         this.age = age;
     }
-
+    public void setGender(char gender){ this.gender = gender;}
 
     public void printInfo(){
         //edit info Frame
@@ -65,71 +62,64 @@ public class Bed extends JButton{
 
         // labels with the bed information using methods from class Bed
         JLabel bedIdLabel = new JLabel("Bed ID: "+this.getID(),SwingConstants.CENTER);
-        JLabel genderLabel = new JLabel("\n Gender: "+this.getGender(),SwingConstants.CENTER);
-        JLabel ageLabel = new JLabel("\n Age: "+this.getAge(),SwingConstants.CENTER);
-
+        JLabel ageLabel = new JLabel("Age: "+this.getAge(),SwingConstants.CENTER);
+        JLabel genderLabel = new JLabel("Gender: "+this.getGender(),SwingConstants.CENTER);
 
         //jbutton for editing bed
         JButton editButton = new JButton("Edit Patient Info");
+        // when button to edit information is clicked
         editButton.addActionListener(new ActionListener() {
-            JTextField ageTextField;
-            JTextField genderTextField;
-            JButton confirmButton;
-
             @Override
             public void actionPerformed(ActionEvent evt) {
-                JFrame editorFrame = new JFrame();
-                editorFrame.setSize(300,300);
-                editorFrame.setBackground(Color.WHITE);
-                editorFrame.setVisible(true);
-                editorFrame.setLocation(300,300);
-
-                JPanel editorPanel = new JPanel();
-                editorPanel.setLayout(new GridLayout(3,1));
-                editorPanel.setBackground(Color.WHITE);
-
-                ageTextField = new JTextField("new age");
-                genderTextField = new JTextField("new gender");
-
-                confirmButton = new JButton("Confirm Edits");
-                confirmButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent evt) {
-                        age = Integer.parseInt(ageTextField.getText());
-                        gender = genderTextField.getText().charAt(0);
-                        printInfo();
-                    }
-
-                });
-
-                editorPanel.add(ageTextField);
-                editorPanel.add(genderTextField);
-                editorPanel.add(confirmButton);
-                editorPanel.setVisible(true);
-                editorFrame.add(editorPanel);
-
+                inputNewInfo();
             }
         });
-
-        //this.setAge(newAge);
 
         // add the labels to a panel that will be added to the frame
         JPanel infoPanel = new JPanel();
         infoPanel.setLayout(new GridLayout(4,1));
         infoPanel.add(bedIdLabel);
-        infoPanel.add(genderLabel);
         infoPanel.add(ageLabel);
+        infoPanel.add(genderLabel);
         infoPanel.add(editButton);
-        infoPanel.setVisible(true);
 
 
         infoFrame.add(infoPanel);
-
     }
 
+    // prints text fields, gets info from user and updates gender and age
+    public void inputNewInfo(){
+        JFrame editorFrame = new JFrame();
+        editorFrame.setSize(300,300);
+        editorFrame.setBackground(Color.WHITE);
+        editorFrame.setVisible(true);
+        editorFrame.setLocation(300,300);
 
+        JPanel editorPanel = new JPanel();
+        editorPanel.setLayout(new GridLayout(3,1));
+        editorPanel.setBackground(Color.WHITE);
 
+        JTextField ageTextField = new JTextField("New age");
+        JTextField genderTextField = new JTextField("New gender");
 
+        JButton confirmButton = new JButton("Confirm Edits");
+        confirmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                setAge(Integer.parseInt(ageTextField.getText()));
+                setGender(genderTextField.getText().charAt(0));
+                infoFrame.dispose();
+                editorFrame.dispose();
 
+                System.out.print(getAge());
+            }
+        });
+
+        editorPanel.add(ageTextField);
+        editorPanel.add(genderTextField);
+        editorPanel.add(confirmButton);
+        editorPanel.setVisible(true);
+        editorFrame.add(editorPanel);
+    }
 
 }
