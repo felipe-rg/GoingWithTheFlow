8807691c9ffe910faf4a546bd0bc1ea1a62596b4
Nicholas Simpson <nicksimpson@nicholass-mbp.home>
2020-12-27 11:53:28 +0000

    Tidy up controlcentre refresh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import Client.*;
 import Methods.AMCWard;
 import Methods.AandE;
+import Methods.ControlCentre;
 import com.google.gson.Gson;
 import Client.Patient;
 import java.io.IOException;
@@ -10,9 +11,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main (String[] args) throws IOException, SQLException {
 
-    Client c = new Client();
-    AMCWard amc = new AMCWard(1);
-    amc.ripPatient(43);
+    ControlCentre methods = new ControlCentre();
+    methods.getAmcCapacityPerc();
     }
 
 }
