da1f4667cdbd4928fea883b0e0991ec8404685fd
nv2917 <nv2917@ic.ac.uk>
2020-12-28 17:35:58 +0000

    Client ver 1.0 including: makeGetRequest makePostRequest makePutRequest makeDeleteRequest
    Other Classes:
    Patient
    Bed
    Ward

---
+++ src/main/java/Client/Bed.java
@@ -0,0 +1,27 @@
+package Client;
+
+import java.io.Serializable;
+
+public class Bed implements Serializable {
+
+    private int bedId;
+    private int wardId;
+    private String status;
+    private String forSex;
+    private boolean hasSideRoom;
+
+    public Bed(int bedId,int wardId,String status,String forSex,boolean hasSideRoom) {
+        this.bedId=bedId;
+        this.wardId=wardId;
+        this.status=status;
+        this.forSex=forSex;
+        this.hasSideRoom=hasSideRoom;
+    }
+
+    public int getBedId(){return bedId;}
+    public int getWardId(){return wardId;}
+    public String getStatus(){return status;}
+    public String getForSex(){return forSex;}
+    public boolean getHasSideRoom(){return hasSideRoom;}
+}
+
